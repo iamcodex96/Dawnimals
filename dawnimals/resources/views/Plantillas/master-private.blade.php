cc65986ecbc9294941c432dab69e13a5d4c433f7
iamcodex96 <Carl.justinev@gmail.com>
2019-02-22 20:24:53 +0100

    Early templates

--- dawnimals/resources/views/Plantillas/master-private.blade.php
+++ dawnimals/resources/views/Plantillas/master-private.blade.php
@@ -4,9 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('Bootstrap/css/bootstrap.min.css') }}">
+    @yield('css_propio')
+    <script src="{{ asset('Bootstrap/js/jquery-3.3.1.min.js') }}"></script>
+    <script src="{{ asset('Bootstrap/js/bootstrap.min.js') }}"></script>
+    @yield('js_onload')
     <title>Document</title>
 </head>
 <body>
 
+    @yield('contenido')
+
+    @yield('js_loaded')
 </body>
 </html>
