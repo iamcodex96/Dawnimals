69c54db60a3aaff26282b71e1efa57fe073e692e
Marc <marc.gonzalez.sds@gmail.com>
2019-02-23 15:51:57 +0100

    Fix all + Secciones

--- resources/views/backend.blade.php
+++ resources/views/backend.blade.php
@@ -11,7 +11,7 @@
     <!-- Bootstrap CSS CDN -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
     <!-- Our Custom CSS -->
-    <link rel="stylesheet" href="/Dawnimals/Dawnimals/public/css/dashboard.css">
+    <link rel="stylesheet" href="/Dawnimals/public/css/dashboard.css">
     <!-- Scrollbar Custom CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.min.css">
 
