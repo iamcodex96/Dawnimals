ef429361dc4eca45300cdcea1560b83552ce9162
EstherGaldeano <33326304+EstherGaldeano@users.noreply.github.com>
2019-02-23 11:24:12 +0100

    Login

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Dawnimals
 Proyecto Protectora sp∀m
 Para ejecutar el proyecto -> php artisan serve
+Imágenes de animales Creative Commons:  http://pngimg.com/imgs/animals/
--- dawnimals/resources/views/Plantillas/master-private.blade.php
+++ dawnimals/resources/views/Plantillas/master-private.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{ asset('Bootstrap/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('Bootstrap/css/bootstrap_login.min.css') }}">
+    
     @yield('css_propio')
     <script src="{{ asset('Bootstrap/js/jquery-3.3.1.min.js') }}"></script>
     <script src="{{ asset('Bootstrap/js/bootstrap.min.js') }}"></script>
