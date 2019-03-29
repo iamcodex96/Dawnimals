286f18deaaebe7df72ca8d3b15e2d22e9a085825
iamcodex96 <Carl,justinev@gmail.com>
2019-03-29 11:47:06 +0100

    Donacion Relation

---
+++ app/Http/Controllers/DonacionController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Donacion;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class DonacionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Donacion  $donacion
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Donacion $donacion)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Donacion  $donacion
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Donacion $donacion)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Donacion  $donacion
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Donacion $donacion)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Donacion  $donacion
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Donacion $donacion)
+    {
+        //
+    }
+}
