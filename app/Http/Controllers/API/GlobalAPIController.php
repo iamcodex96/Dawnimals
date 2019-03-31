aea50c5b039593a6f04fcea262bbf6de2a5bff88
iamcodex96 <Carl.justinev@gmail.com>
2019-03-31 19:55:33 +0200

    Create GlobalAPIController.php

---
+++ app/Http/Controllers/API/GlobalAPIController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Models\Donante;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\DonanteResource;
+use Illuminate\Database\QueryException;
+use App\Classes\Utilitat;
+
+
+class GlobalAPIController extends Controller{
+
+    public function getTipos(){
+
+    }
+    public function getSubTiposByTipo($tipo){
+
+    }
+
+}
