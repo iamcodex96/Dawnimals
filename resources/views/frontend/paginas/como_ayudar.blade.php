acf18946fd6cdf56c1bef97aba48f0f4c3b94205
iamcodex96 <Carl.justinev@gmail.com>
2019-04-22 19:43:00 +0200

    Como-ayudar Finish

--- resources/views/frontend/paginas/como_ayudar.blade.php
+++ resources/views/frontend/paginas/como_ayudar.blade.php
@@ -3,6 +3,28 @@
 @section('css_propio')
 <link rel="stylesheet" href="{{ asset('css/frontend/quien-somos.css') }}">
 <link rel="stylesheet" href="{{ asset('css/frontend/como-ayudar.css') }}">
+<style>
+.hideme{
+    opacity: 0;
+}
+.btn-spam{
+    position: relative;
+    left: 20%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    outline-width: 0px !important;
+    user-select: auto !important;
+    font-size: 30px;
+    height: 100px;
+    width: 60%;
+    background-color: #636;
+    color: white;
+}
+.btn-spam:hover{
+    color: white;
+}
+</style>
 @endsection
 
 @section('titulo')
@@ -13,7 +35,7 @@
 <main class="container-fluid">
         {{-- <img src="{{ asset('img/quien-somos/img-quien-somos-8.png') }}" class="img-fluid" alt="Responsive image"> --}}
         <div class="text-center mt-5 mb-3">
-                <h1>¿Cómo puedo ayudar?</h1>
+                <h1 id="typer"></h1>
         </div>
 
         <div class="col-12 text-center video-links2" height="1000px">
@@ -42,19 +64,59 @@
 
             </div>
 
-    <div class="text-center m-5">
-        <h1>Dona dinero</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipiscing elit cras varius sollicitudin, taciti vel mollis nunc pretium netus sodales volutpat luctus conubia neque, convallis orci suspendisse leo integer libero commodo inceptos curae. Velit pellentesque curabitur eget aliquet cursus lectus ad maecenas suscipit pulvinar congue luctus ultricies, curae porta vulputate lobortis neque hac porttitor potenti dui senectus sollicitudin integer. Condimentum diam eu fermentum nam metus platea neque in mattis tempus dis tempor, ad sed molestie mi porta blandit bibendum tortor arcu proin hendrerit.
-
-                Eros tincidunt duis tempor ante potenti molestie libero blandit commodo euismod per litora sapien, quis mattis turpis maecenas elementum cum suscipit quisque platea eu orci. Pharetra purus consequat vitae tempus mi proin maecenas nulla, dictumst at eu posuere condimentum aliquam primis etiam, vel augue quisque cubilia euismod netus vestibulum. Laoreet varius accumsan elementum per vehicula purus phasellus quam, nibh fames maecenas donec ligula felis euismod duis, at parturient turpis egestas auctor fermentum integer.</p>
+    <div class="text-center m-5 hideme">
+        <h1>{{__("frontend.how-dona-amor")}}</h1>
+        <p>{{__("frontend.how-text-amor")}}</p>
     </div>
     <div class="row p-5">
+        <div class="col-md-6 hideme">
+                <img src="{{ asset('img/quien-somos/wooooh.jpg') }}" class="img-fluid" alt="Responsive image">
+        </div>
         <div class="col-md-6">
+            <h2>{{__("frontend.how-requisitos")}}</h2>
+            <p>{{__("frontend.how-requisitos-txt")}}</p>
+            <h2>{{__("frontend.how-coste")}}</h2>
+            <p>{{__("frontend.how-coste-txt01")}}
+            <br>{{__("frontend.how-coste-txt02")}}
+            <br>{{__("frontend.how-coste-txt03")}}
+            <br>{{__("frontend.how-coste-txt04")}}
+            <br>{{__("frontend.how-coste-txt05")}}
+            <br>{{__("frontend.how-coste-txt06")}}
+            <br>{{__("frontend.how-coste-txt07")}}
+            </p>
+            <a href="https://www.protectoramataro.org/ca/cercador-animals" class="btn btn-lg btn-spam">Adopta</a>
+        </div>
+    </div>
+    <div class="text-center m-5 hideme">
+            <h1>{{__("frontend.how-dona-tiempo")}}</h1>
+            <p>{{__("frontend.how-dona-tiempo-texto")}}</p>
+        </div>
+        <div class="row p-5">
+            <div class="col-md-6 hideme">
+                <h2>{{__("frontend.how-voluntario")}}</h2>
+                <p>{{__("frontend.how-voluntario-txt01")}}</p>
+                <p>{{__("frontend.how-voluntario-txt02")}}</p>
+            </div>
+            <div class="col-md-6">
+                    <img src="{{ asset('img/quien-somos/happy.jpg') }}" class="img-fluid hideme" alt="Responsive image">
+            </div>
+        </div>
+    <div class="text-center m-5 hideme">
+        <h1>{{__("frontend.how-dona-dinero")}}</h1>
+        <p>{{__("frontend.how-dona-dinero-txt")}}</p>
+    </div>
+    <div class="row p-5">
+        <div class="col-md-6 hideme">
                 <img src="{{ asset('img/quien-somos/antua-pics-01.jpg') }}" class="img-fluid" alt="Responsive image">
         </div>
         <div class="col-md-6">
-            <button class="btn btn-lg btn-primary fb"><i class="fab fa-facebook-f"></i> Dona en Facebook</button>
-            <button class="btn btn-lg btn-success fb"><i class="fas fa-hand-holding-usd"></i> Participa en els reptes</button>
+            <div style="position: relative; top: 15%; color:white">
+                <a href="https://www.facebook.com/protectoramataro" class="btn btn-lg btn-primary m-1 w-100 hideme"><i class="fab fa-facebook-f"></i>{{__("frontend.how-facebook")}}</a>
+                <a href="" class="btn btn-lg btn-success m-1 w-100 hideme"><i class="fas fa-hand-holding-usd"></i>{{__("frontend.how-grano")}}</a>
+                <a href="https://www.protectoramataro.org/es/necesitamos-socios" class="btn btn-lg btn-dark m-1 w-100 hideme"><i class="fas fa-hand-holding-usd"></i>{{__("frontend.how-socio")}}</a>
+                <a href="" class="btn btn-lg btn-warning m-1 w-100 hideme"><i class="fas fa-hand-holding-usd"></i>{{__("frontend.how-padrino")}}</a>
+                <a href="" class="btn btn-lg btn-secondary m-1 w-100 hideme"><i class="fas fa-hand-holding-usd"></i>{{__("frontend.how-teaming")}}</a>
+            </div>
         </div>
     </div>
 </main>
@@ -65,15 +127,41 @@
 {{-- <script src="{{ asset('./js/frontend/landing.js') }}"></script> --}}
 <script src="{{ asset('./js/frontend/video.js') }}"></script>
 <script>
-    var $spam = $('#spam');
+    var typer = $('#typer');
+    var txt = '{{__("frontend.toggle_ayudar")}}';
+    var textoAmostrar='';
+    var speed = 50;
+    var i=0;
+    var j=0;
+    typerEffect();
 
-    $(window).scroll(function(){
-        console.log($(this).scrollTop());
-    })
+    $(document).ready(function() {
+        $(window).scroll( function(){
+                $('.hideme').each( function(i){
 
-    function showOnScroll(divId){
-        var divMostrar = $('#'+divId);
-    }
+                    var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+                    var bottom_of_window = $(window).scrollTop() + $(window).height();
 
+                    if( bottom_of_window+200 > bottom_of_object ){
+
+                        $(this).animate({'opacity':'1'},500);
+
+                    }
+
+                });
+
+            });
+
+        });
+
+
+    function typerEffect(){
+        if (i < txt.length){
+            textoAmostrar+= txt.charAt(i)
+            typer.html(textoAmostrar);
+            i++;
+            setTimeout(typerEffect, speed);
+        }
+    };
 </script>
 @endsection
